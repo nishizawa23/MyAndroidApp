1403e0da22c163015eed6049e7827c54f2a00b57
pete <nishizawa23@gmail.com>
2014-10-02 22:51:42 +0800

    add demo for start a activity.

--- MyAndroidApp/src/alpha/demo/myandroidapp/MainActivity.java
+++ MyAndroidApp/src/alpha/demo/myandroidapp/MainActivity.java
@@ -1,11 +1,16 @@
 package alpha.demo.myandroidapp;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
+
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +35,16 @@ public class MainActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	/** Called when the user clicks the Send button */
+	public void sendMessage(View view) {
+
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
+
 	}
 }
